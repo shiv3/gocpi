3e3e3db93663005051dd0c64d7388cbb4a344b0a
shiv3 <tshibaza@gmail.com>
2026-06-24 13:06:55 +0900

    feat(pricing): Calculate engine (pooling, tax rollup, clamp, flat-once)

--- pricing/engine.go
+++ pricing/engine.go
@@ -1,5 +1,16 @@
 package pricing
 
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+const (
+	energyBaseUnitsPerKwh = 1000
+	timeBaseUnitsPerHour  = 3600
+)
+
 type componentSet struct {
 	energy, time, parking, flat *PriceComponent
 }
@@ -53,4 +64,233 @@ func activeComponents(tariff Tariff, start snapshot, p Period) (componentSet, []
 	}
 
 	return cs, warns
+}
+
+func ValidateInput(in Input) error {
+	return nil
+}
+
+func currencyScale(code string) int {
+	return 2
+}
+
+func Calculate(in Input, opts Options) (Report, error) {
+	if err := ValidateInput(in); err != nil {
+		return Report{}, err
+	}
+
+	loc := opts.TimeZone
+	if loc == nil {
+		loc = time.UTC
+	}
+
+	precision := currencyScale(in.Currency)
+	if opts.CurrencyPrecision != nil {
+		precision = *opts.CurrencyPrecision
+	}
+
+	rep := Report{
+		Dimensions: make(map[DimensionType]Dimension),
+	}
+	rep.Warnings = append(rep.Warnings, tariffWindowWarnings(in)...)
+
+	energyPools := make(map[*PriceComponent]decimal.Decimal)
+	timePools := make(map[*PriceComponent]decimal.Decimal)
+	parkingPools := make(map[*PriceComponent]decimal.Decimal)
+	var flatComp *PriceComponent
+
+	cur := newSnapshot(in.Start, loc)
+	for i, period := range in.Periods {
+		end := in.End
+		if i+1 < len(in.Periods) {
+			end = in.Periods[i+1].Start
+		}
+
+		startSnap := cur
+		cs, warns := activeComponents(in.Tariff, startSnap, period)
+		rep.Warnings = append(rep.Warnings, warns...)
+
+		if cs.flat != nil && flatComp == nil {
+			flatComp = cs.flat
+		}
+
+		matchedAny := componentSetHasAny(cs)
+		periodHasVolume := false
+		if period.Energy != nil {
+			periodHasVolume = true
+			if cs.energy != nil {
+				addPool(energyPools, cs.energy, *period.Energy)
+			} else if matchedAny {
+				rep.Warnings = append(rep.Warnings, noElementWarning("no ENERGY component for period volume"))
+			}
+		}
+		if period.Time != nil {
+			periodHasVolume = true
+			if cs.time != nil {
+				addPool(timePools, cs.time, *period.Time)
+			} else if matchedAny {
+				rep.Warnings = append(rep.Warnings, noElementWarning("no TIME component for period volume"))
+			}
+		}
+		if period.ParkingTime != nil {
+			periodHasVolume = true
+			if cs.parking != nil {
+				addPool(parkingPools, cs.parking, *period.ParkingTime)
+			} else if matchedAny {
+				rep.Warnings = append(rep.Warnings, noElementWarning("no PARKING_TIME component for period volume"))
+			}
+		}
+
+		if periodHasVolume && !matchedAny {
+			rep.Warnings = append(rep.Warnings, noElementWarning("period matched no tariff element"))
+		}
+
+		cur = cur.next(period, end)
+	}
+
+	var err error
+	rep.TotalEnergyCost, rep.Dimensions[Energy], err = pricePooledDimension(energyPools, decimal.NewFromInt(energyBaseUnitsPerKwh))
+	if err != nil {
+		return Report{}, err
+	}
+	rep.TotalTimeCost, rep.Dimensions[Time], err = pricePooledDimension(timePools, decimal.NewFromInt(timeBaseUnitsPerHour))
+	if err != nil {
+		return Report{}, err
+	}
+	rep.TotalParkingCost, rep.Dimensions[ParkingTime], err = pricePooledDimension(parkingPools, decimal.NewFromInt(timeBaseUnitsPerHour))
+	if err != nil {
+		return Report{}, err
+	}
+	rep.TotalFixedCost, rep.Dimensions[Flat] = priceFlatDimension(flatComp)
+
+	rep.TotalCost.BeforeTaxes = rep.TotalEnergyCost.BeforeTaxes.
+		Add(rep.TotalTimeCost.BeforeTaxes).
+		Add(rep.TotalParkingCost.BeforeTaxes).
+		Add(rep.TotalFixedCost.BeforeTaxes)
+	rep.TotalCost.BeforeTaxes = clampTotalBeforeTaxes(rep.TotalCost.BeforeTaxes, in.Tariff)
+
+	rep.TotalEnergyCost = roundMoney(rep.TotalEnergyCost, precision)
+	rep.TotalTimeCost = roundMoney(rep.TotalTimeCost, precision)
+	rep.TotalParkingCost = roundMoney(rep.TotalParkingCost, precision)
+	rep.TotalFixedCost = roundMoney(rep.TotalFixedCost, precision)
+	rep.TotalCost = roundMoney(rep.TotalCost, precision)
+	rep.Dimensions[Energy] = Dimension{Volume: rep.Dimensions[Energy].Volume, Cost: rep.TotalEnergyCost}
+	rep.Dimensions[Time] = Dimension{Volume: rep.Dimensions[Time].Volume, Cost: rep.TotalTimeCost}
+	rep.Dimensions[ParkingTime] = Dimension{Volume: rep.Dimensions[ParkingTime].Volume, Cost: rep.TotalParkingCost}
+	rep.Dimensions[Flat] = Dimension{Volume: rep.Dimensions[Flat].Volume, Cost: rep.TotalFixedCost}
+
+	return rep, nil
+}
+
+func addPool(pools map[*PriceComponent]decimal.Decimal, comp *PriceComponent, volume decimal.Decimal) {
+	pools[comp] = pools[comp].Add(volume)
+}
+
+func componentSetHasAny(cs componentSet) bool {
+	return cs.energy != nil || cs.time != nil || cs.parking != nil || cs.flat != nil
+}
+
+func tariffWindowWarnings(in Input) []Warning {
+	var warns []Warning
+	if in.Tariff.StartDateTime != nil && in.Start.Before(*in.Tariff.StartDateTime) {
+		warns = append(warns, Warning{
+			Code: WarnTariffWindow,
+			Kind: KindWarning,
+			Msg:  "session starts before tariff validity window",
+		})
+	}
+	if in.Tariff.EndDateTime != nil && in.End.After(*in.Tariff.EndDateTime) {
+		warns = append(warns, Warning{
+			Code: WarnTariffWindow,
+			Kind: KindWarning,
+			Msg:  "session ends after tariff validity window",
+		})
+	}
+	return warns
+}
+
+func noElementWarning(msg string) Warning {
+	return Warning{
+		Code: WarnNoElement,
+		Kind: KindWarning,
+		Msg:  msg,
+	}
+}
+
+func pricePooledDimension(pools map[*PriceComponent]decimal.Decimal, baseUnitsPerUnit decimal.Decimal) (Money, Dimension, error) {
+	totalBefore := decimal.Zero
+	totalVolume := decimal.Zero
+	totalTax := decimal.Zero
+	allTaxesKnown := true
+	anyTaxesKnown := false
+	var firstComp *PriceComponent
+	componentCount := 0
+
+	for comp, volume := range pools {
+		if firstComp == nil {
+			firstComp = comp
+		}
+		componentCount++
+		totalVolume = totalVolume.Add(volume)
+
+		baseVol := volume.Mul(baseUnitsPerUnit)
+		billed, err := stepBill(baseVol, decimal.NewFromInt(int64(comp.StepSize)))
+		if err != nil {
+			return Money{}, Dimension{}, err
+		}
+		costVol := billed.Div(baseUnitsPerUnit)
+		subtotalBefore := roundOCPI(costVol.Mul(comp.Price))
+		totalBefore = totalBefore.Add(subtotalBefore)
+
+		after, ok := (Money{BeforeTaxes: subtotalBefore, Taxes: comp.Taxes}).afterTax()
+		if ok {
+			anyTaxesKnown = true
+			totalTax = totalTax.Add(after.Sub(subtotalBefore))
+		} else {
+			allTaxesKnown = false
+		}
+	}
+
+	money := Money{BeforeTaxes: totalBefore}
+	if componentCount == 1 {
+		money.Taxes = firstComp.Taxes
+	}
+	if componentCount != 1 && anyTaxesKnown && allTaxesKnown {
+		money.Taxes = []TaxAmount{{Amount: &totalTax}}
+	}
+
+	return money, Dimension{Volume: totalVolume, Cost: money}, nil
+}
+
+func priceFlatDimension(comp *PriceComponent) (Money, Dimension) {
+	if comp == nil {
+		money := Money{BeforeTaxes: decimal.Zero}
+		return money, Dimension{Cost: money}
+	}
+
+	money := Money{
+		BeforeTaxes: roundOCPI(comp.Price),
+		Taxes:       comp.Taxes,
+	}
+
+	return money, Dimension{Volume: decimal.NewFromInt(1), Cost: money}
+}
+
+func clampTotalBeforeTaxes(total decimal.Decimal, tariff Tariff) decimal.Decimal {
+	if tariff.MinPrice != nil && total.LessThan(tariff.MinPrice.BeforeTaxes) {
+		return tariff.MinPrice.BeforeTaxes
+	}
+	if tariff.MaxPrice != nil && total.GreaterThan(tariff.MaxPrice.BeforeTaxes) {
+		return tariff.MaxPrice.BeforeTaxes
+	}
+	return total
+}
+
+func roundMoney(m Money, precision int) Money {
+	m.BeforeTaxes = roundCurrency(m.BeforeTaxes, precision)
+	if m.AfterTaxes != nil {
+		after := roundCurrency(*m.AfterTaxes, precision)
+		m.AfterTaxes = &after
+	}
+	return m
 }
---
+++ pricing/engine_calculate_test.go
@@ -0,0 +1,102 @@
+package pricing
+
+import (
+	"testing"
+	"time"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func ptrInt(i int) *int { return &i }
+
+func TestCalculate(t *testing.T) {
+	d := decimal.RequireFromString
+	dp := func(s string) *decimal.Decimal { v := d(s); return &v }
+	utc := time.UTC
+	start := time.Date(2026, 6, 24, 9, 0, 0, 0, utc)
+
+	baseTariff := func() Tariff {
+		return Tariff{Currency: "EUR", Elements: []Element{{Components: []PriceComponent{
+			{Type: Flat, Price: d("1.00"), StepSize: 1},
+			{Type: Energy, Price: d("0.30"), StepSize: 1},
+			{Type: Time, Price: d("2.00"), StepSize: 1},
+		}}}}
+	}
+	mkIn := func(tf Tariff, periods []Period, end time.Time) Input {
+		return Input{Version: V221, Currency: "EUR", Start: start, End: end, Tariff: tf, Periods: periods}
+	}
+
+	t.Run("energy_time_flat_total", func(t *testing.T) {
+		in := mkIn(baseTariff(), []Period{{Start: start, Energy: dp("10"), Time: dp("1.0"), MaxPower: dp("11")}}, start.Add(time.Hour))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.True(t, rep.TotalEnergyCost.BeforeTaxes.Equal(d("3.00")))
+		assert.True(t, rep.TotalTimeCost.BeforeTaxes.Equal(d("2.00")))
+		assert.True(t, rep.TotalFixedCost.BeforeTaxes.Equal(d("1.00")))
+		assert.True(t, rep.TotalCost.BeforeTaxes.Equal(d("6.00")))
+	})
+
+	t.Run("flat_once_across_periods", func(t *testing.T) {
+		in := mkIn(baseTariff(), []Period{
+			{Start: start, Energy: dp("5"), Time: dp("0.5"), MaxPower: dp("11")},
+			{Start: start.Add(30 * time.Minute), Energy: dp("5"), Time: dp("0.5"), MaxPower: dp("11")},
+		}, start.Add(time.Hour))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.True(t, rep.TotalFixedCost.BeforeTaxes.Equal(d("1.00")), "flat once, got %s", rep.TotalFixedCost.BeforeTaxes)
+	})
+
+	t.Run("step_pooled_across_session", func(t *testing.T) {
+		// ENERGY step 1000 Wh; two periods 0.4kWh + 0.4kWh = 0.8kWh -> pooled 800Wh -> ceil to 1000Wh
+		tf := Tariff{Currency: "EUR", Elements: []Element{{Components: []PriceComponent{{Type: Energy, Price: d("0.30"), StepSize: 1000}}}}}
+		in := mkIn(tf, []Period{
+			{Start: start, Energy: dp("0.4"), MaxPower: dp("11")},
+			{Start: start.Add(30 * time.Minute), Energy: dp("0.4"), MaxPower: dp("11")},
+		}, start.Add(time.Hour))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.True(t, rep.TotalEnergyCost.BeforeTaxes.Equal(d("0.30")), "1kWh billed, got %s", rep.TotalEnergyCost.BeforeTaxes)
+	})
+
+	t.Run("min_max_price_clamp", func(t *testing.T) {
+		tf := baseTariff()
+		tf.MinPrice = &Money{BeforeTaxes: d("10.00")}
+		in := mkIn(tf, []Period{{Start: start, Energy: dp("1"), Time: dp("0.1"), MaxPower: dp("11")}}, start.Add(6*time.Minute))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.True(t, rep.TotalCost.BeforeTaxes.Equal(d("10.00")), "clamped to min, got %s", rep.TotalCost.BeforeTaxes)
+	})
+
+	t.Run("missing_dimension_zero", func(t *testing.T) {
+		// no ENERGY dimension in period -> energy cost 0, warning
+		in := mkIn(baseTariff(), []Period{{Start: start, Time: dp("1.0"), MaxPower: dp("11")}}, start.Add(time.Hour))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.True(t, rep.TotalEnergyCost.BeforeTaxes.Equal(d("0")))
+	})
+
+	t.Run("tax_rollup_before_after", func(t *testing.T) {
+		tf := Tariff{Currency: "EUR", Elements: []Element{{Components: []PriceComponent{
+			{Type: Energy, Price: d("0.30"), StepSize: 1, Taxes: []TaxAmount{{Percent: dp("20")}}},
+		}}}}
+		in := mkIn(tf, []Period{{Start: start, Energy: dp("10"), MaxPower: dp("11")}}, start.Add(time.Hour))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.True(t, rep.TotalEnergyCost.BeforeTaxes.Equal(d("3.00")))
+		after, ok := rep.TotalEnergyCost.afterTax()
+		require.True(t, ok)
+		assert.True(t, after.Equal(d("3.60")), "20%% VAT, got %s", after)
+	})
+
+	t.Run("tariff_window_warns", func(t *testing.T) {
+		tf := baseTariff()
+		win := start.Add(48 * time.Hour)
+		tf.StartDateTime = &win // session starts before tariff valid
+		in := mkIn(tf, []Period{{Start: start, Energy: dp("1"), MaxPower: dp("11")}}, start.Add(time.Hour))
+		rep, err := Calculate(in, Options{CurrencyPrecision: ptrInt(2)})
+		require.NoError(t, err)
+		assert.NotEmpty(t, rep.Warnings)
+	})
+}
