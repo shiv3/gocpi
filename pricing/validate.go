79d6b23f59f30b5fab496ac3a6357b4444e6e218
shiv3 <tshibaza@gmail.com>
2026-06-24 13:47:26 +0900

    fix(pricing): allow step_size 0 on FLAT components (conformance)

--- pricing/validate.go
+++ pricing/validate.go
@@ -17,7 +17,7 @@ func ValidateInput(in Input) error {
 	for i := range in.Tariff.Elements {
 		el := &in.Tariff.Elements[i]
 		for j := range el.Components {
-			if el.Components[j].StepSize <= 0 {
+			if (el.Components[j].Type == Energy || el.Components[j].Type == Time || el.Components[j].Type == ParkingTime) && el.Components[j].StepSize <= 0 {
 				return invalidInput("tariff element %d price component %d has non-positive step_size %d", i, j, el.Components[j].StepSize)
 			}
 		}
